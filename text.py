c1db46d26bda5d4f2cf98c657166cf17d61ed4f5
htv <htv@continia.com>
2022-05-20 17:34:51 +0200

    OS

--- text.py
+++ text.py
@@ -1,13 +1,18 @@
-import csv
+import csv, os
+from fileinput import filename
+
+TEMPLATE_DIR = "templates"
+OUTPUT_DIR = "outputfiles"
+VARIABLES_DIR ="variables"
 
 def readtemplate():
-    with open('template.xml', 'r', encoding='utf8') as f:
+    with open(os.path.join(TEMPLATE_DIR,"template.xml"), 'r', encoding='utf8') as f:
         data = f.read()
         return data
 
 def readmapping():
     countrylist = []
-    with open('variables.csv', newline="", encoding="utf-8-sig") as file:
+    with open(os.path.join(VARIABLES_DIR,"variables.csv"), newline="", encoding="utf-8-sig") as file:
         csv_reader = csv.DictReader(file, delimiter=";")
         countrylist = csv_reader.fieldnames[1:]
         documents = {}
@@ -19,7 +24,8 @@ def readmapping():
                 documents[country] = documents[country].replace(
                     f'<!-- {row["KEY"]}-->', row[country])
         for country in countrylist:
-            with open(f'outputfiles/{country}-inv1.xml', "w", encoding="utf-8-sig") as file:
+            filename = f'{country}-inv1.xml'
+            with open(os.path.join(OUTPUT_DIR,filename), "w", encoding="utf-8-sig") as file:
                 file.write(documents[country])
         print(documents["ES"])
 
