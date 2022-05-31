931bfb53ad934966c6fd767662476da3e7d58af2
htv <htv@continia.com>
2022-05-31 19:22:25 +0200

    excel sheet

--- createfiles.py
+++ createfiles.py
@@ -1,4 +1,4 @@
-import csv, os, yaml, datetime, base64
+import csv, os, yaml, datetime, base64, pandas
 
 def loadsettings():
     with open("settings.yaml","r",encoding="utf-8") as f:
@@ -10,7 +10,6 @@ FILES_DIR = "files"
 ACTIVE_DIR = settings["ACTIVE_DIR"]
 TEMPLATE_FILE = settings["TEMPLATE_FILE"]
 VARIABLES_FILE = settings["VARIABLES_FILE"]
-CSV_DELIMITER = settings["CSV_DELIMITER"]
 
 def getbase64string(path):
     with open(path, "rb") as pdf_file:
@@ -24,28 +23,24 @@ def readtemplate():
         return data
 
 def createfiles():
-    countrylist = []
-    with open(os.path.join(FILES_DIR,ACTIVE_DIR,VARIABLES_FILE), newline="", encoding="utf-8-sig") as f:
-        csv_reader = csv.DictReader(f, delimiter=CSV_DELIMITER)
-        countrylist = csv_reader.fieldnames[1:]
-        documents = {}
-        template = readtemplate()
-        for country in countrylist:
-            documents[country] = template
-        for row in csv_reader:
-            for country in countrylist:
-                documents[country] = documents[country].replace(
-                    f'<!-- {row["KEY"]}-->', row[country])
-                pdffile = os.path.join(FILES_DIR,ACTIVE_DIR,f'{country}.pdf')
-                if os.path.exists(pdffile):
-                    encodedpdf = getbase64string(pdffile)
-                    documents[country] = documents[country].replace("<!-- ATTACHMENT-->",encodedpdf)
-        #Generer oversatte filer
-        outputdirname = TEMPLATE_FILE + " " + datetime.datetime.now().strftime("%m-%d-%Y %H %M %S")
-        os.mkdir(os.path.join(FILES_DIR,ACTIVE_DIR,outputdirname))
-        for country in countrylist:
-            filename = f'{country} - {TEMPLATE_FILE}'
-            with open(os.path.join(FILES_DIR,ACTIVE_DIR,outputdirname,filename), "w", encoding="utf-8-sig") as f:
-                f.write(documents[country])
+    df = pandas.read_excel(os.path.join(FILES_DIR,ACTIVE_DIR,VARIABLES_FILE),index_col=0,keep_default_na=False)
+    dictdata = df.to_dict()
+    documents = {}
+    template = readtemplate()
+    outputdirname = TEMPLATE_FILE + " " + datetime.datetime.now().strftime("%m-%d-%Y %H %M %S")
+    os.mkdir(os.path.join(FILES_DIR, ACTIVE_DIR,outputdirname))
+    for country, dict_country in dictdata.items():
+        documents[country] = template
+        for key_translation, value_translation in dict_country.items():
+            documents[country] = documents[country].replace(
+                f'<!-- {key_translation}-->', str(value_translation))
+        pdffile = os.path.join(FILES_DIR,ACTIVE_DIR,f'{country}.pdf')
+        if os.path.exists(pdffile):
+            encodedpdf = getbase64string(pdffile)
+            documents[country] = documents[country].replace("<!-- ATTACHMENT-->",encodedpdf)
+    #Generer oversatte filer
+        filename = f'{country} - {TEMPLATE_FILE}'
+        with open(os.path.join(FILES_DIR,ACTIVE_DIR,outputdirname,filename), "w", encoding="utf-8-sig") as f:
+            f.write(documents[country])
 
 createfiles()
