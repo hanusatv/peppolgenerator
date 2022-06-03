0f48fe3ed6c076c5c067062ee5139cdd08e84e21
hanusatv <hanusatv@gmail.com>
2022-06-03 04:28:07 +0200

    sucessful bach generation with word templates

--- createfiles.py
+++ createfiles.py
@@ -3,6 +3,10 @@ import yaml
 import datetime
 import base64
 import pandas
+import shutil
+import tempfile
+import zipfile
+import docx2pdf
 
 
 def loadsettings():
@@ -14,48 +18,71 @@ def loadsettings():
 settings = loadsettings()
 
 FILES_DIR = 'files'
-ACTIVE_DIR = settings['ACTIVE_DIR']
-XML_TEMPLATE_FILE = settings['XML_TEMPLATE_FILE']
-WORD_TEMPLATE_FILE = settings['WORD_TEMPLATE_FILE']
-VARIABLES_FILE = settings['VARIABLES_FILE']
+ACTIVE_DIR = os.path.join(FILES_DIR, settings['ACTIVE_DIR'])
+XML_TEMPLATE_FILE_NAME = settings['XML_TEMPLATE_FILE']
+XML_TEMPLATE_FILE = os.path.join(ACTIVE_DIR, settings['XML_TEMPLATE_FILE'])
+WORD_TEMPLATE_FILE = os.path.join(ACTIVE_DIR, settings['WORD_TEMPLATE_FILE'])
+XML_VARIABLES_FILE = os.path.join(ACTIVE_DIR, settings['XML_VARIABLES_FILE'])
+WORD_VARIABLES_FILE = os.path.join(ACTIVE_DIR, settings['WORD_VARIABLES_FILE'])
 
 
-def getbase64string(path):
-    with open(path, 'rb') as pdf_file:
-        encoded_string = base64.b64encode(pdf_file.read())
-        pure_string = encoded_string.decode('utf-8')
-        return(pure_string)
-
-
-def readtemplate():
-    with open(os.path.join(FILES_DIR, ACTIVE_DIR, XML_TEMPLATE_FILE), 'r', encoding='utf8') as f:
+def readXmlTemplate():
+    with open(XML_TEMPLATE_FILE, 'r', encoding='utf8') as f:
         data = f.read()
         return data
 
+# Make const that is word variables that can be used for each loop by filtering
+
 
 def createfiles():
-    df = pandas.read_excel(os.path.join(
-        FILES_DIR, ACTIVE_DIR, VARIABLES_FILE), index_col=0, keep_default_na=False)
+    df = pandas.read_excel(
+        XML_VARIABLES_FILE, index_col=0, keep_default_na=False)
     dictdata = df.to_dict()
     documents = {}
-    template = readtemplate()
-    outputdirname = XML_TEMPLATE_FILE + ' ' + \
-        datetime.datetime.now().strftime('%m-%d-%Y %H %M %S')
-    os.mkdir(os.path.join(FILES_DIR, ACTIVE_DIR, outputdirname))
+    template = readXmlTemplate()
+    outputdirname = datetime.datetime.now().strftime('%m-%d-%Y %H %M %S')
+    os.mkdir(os.path.join(ACTIVE_DIR, outputdirname))
     for country, dict_country in dictdata.items():
         documents[country] = template
         for key_translation, value_translation in dict_country.items():
             documents[country] = documents[country].replace(
                 f'<!-- {key_translation}-->', str(value_translation))
-        pdffile = os.path.join(FILES_DIR, ACTIVE_DIR, f'{country}.pdf')
-        if os.path.exists(pdffile):
-            encodedpdf = getbase64string(pdffile)
+        if os.path.exists(WORD_VARIABLES_FILE):
+            base64pdf = wordTemplateToBase64(country)
             documents[country] = documents[country].replace(
-                '<!-- ATTACHMENT-->', encodedpdf)
+                '<!-- ATTACHMENT-->', base64pdf)
     # Generer oversatte filer
-        filename = f'{country} - {XML_TEMPLATE_FILE}'
-        with open(os.path.join(FILES_DIR, ACTIVE_DIR, outputdirname, filename), 'w', encoding='utf-8-sig') as f:
+        filename = f'{country} - {XML_TEMPLATE_FILE_NAME}'
+        with open(os.path.join(ACTIVE_DIR, outputdirname, filename), 'w', encoding='utf-8-sig') as f:
             f.write(documents[country])
+
+
+def wordTemplateToBase64(localization):
+    with tempfile.TemporaryDirectory(prefix="peppolgenerator-") as tmpdir:
+        df = pandas.read_excel(WORD_VARIABLES_FILE, index_col=0, usecols=['KEY', localization],
+                               keep_default_na=False)
+        dictdata = df.to_dict()
+        with zipfile.ZipFile(WORD_TEMPLATE_FILE, 'r') as zip_ref:
+            zip_ref.extractall(os.path.join(tmpdir, 'unzipped'))
+        with open(os.path.join(tmpdir, 'unzipped/word/document.xml'), 'r', encoding='utf8') as f:
+            data = f.read()
+        document = data
+        for key_translation, value_translation in dictdata[localization].items():
+            document = document.replace(
+                f'#{key_translation}#', str(value_translation))
+        # Generer oversatte filer
+            with open(os.path.join(tmpdir, 'unzipped/word/document.xml'), 'w', encoding='utf-8-sig') as f:
+                f.write(document)
+        shutil.make_archive(os.path.join(tmpdir, 'zipped'),
+                            'zip', os.path.join(tmpdir, 'unzipped'))
+        os.rename(os.path.join(tmpdir, 'zipped.zip'),
+                  os.path.join(tmpdir, 'zipped.docx'))
+        docx2pdf.convert(os.path.join(tmpdir, 'zipped.docx'),
+                         os.path.join(tmpdir, 'base64encoded.pdf'))
+        with open(os.path.join(tmpdir, 'base64encoded.pdf'), "rb") as pdf_file:
+            encoded_string = base64.b64encode(pdf_file.read())
+        pure_string = encoded_string.decode('utf-8')
+    return pure_string
 
 
 createfiles()
