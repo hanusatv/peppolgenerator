8c82125ab9eb0270d2c38746e6af0e2bd2a42c6f
hanusatv <hanusatv@gmail.com>
2022-06-25 01:35:30 +0200

    pip env requirements-file added

--- createfiles.py
+++ createfiles.py
@@ -10,33 +10,38 @@ import docx2pdf
 
 FILES_DIR = 'files'
 
+
 def loadsettings():
     settings = {}
     with open('settings.yaml', 'r', encoding='utf-8') as f:
         activeDir = yaml.safe_load(f)
         settings['activedir'] = activeDir
-    with open(os.path.join(FILES_DIR, settings['activedir']['ACTIVE_DIR'],'settings.yaml'), 'r', encoding='utf-8') as f:
+    with open(os.path.join(FILES_DIR, settings['activedir']['ACTIVE_DIR'], 'settings.yaml'), 'r', encoding='utf-8') as f:
         activeDirSettings = yaml.safe_load(f)
-        print(activeDirSettings)
         settings['activeDirSettings'] = activeDirSettings
     return settings
+
 
 settings = loadsettings()
 
 ACTIVE_DIR = os.path.join(FILES_DIR, settings['activedir']['ACTIVE_DIR'])
 XML_TEMPLATE_FILE_NAME = settings['activeDirSettings']['XML_TEMPLATE_FILE']
-XML_TEMPLATE_FILE = os.path.join(ACTIVE_DIR, settings['activeDirSettings']['XML_TEMPLATE_FILE'])
-XML_VARIABLES_FILE = os.path.join(ACTIVE_DIR, settings['activeDirSettings']['XML_VARIABLES_FILE'])
+XML_TEMPLATE_FILE = os.path.join(
+    ACTIVE_DIR, settings['activeDirSettings']['XML_TEMPLATE_FILE'])
+XML_VARIABLES_FILE = os.path.join(
+    ACTIVE_DIR, settings['activeDirSettings']['XML_VARIABLES_FILE'])
 if settings['activeDirSettings']['WORD_TEMPLATE_FILE'] != None:
-    WORD_TEMPLATE_FILE = os.path.join(ACTIVE_DIR, settings['activeDirSettings']['WORD_TEMPLATE_FILE'])
-    WORD_VARIABLES_FILE = os.path.join(ACTIVE_DIR, settings['activeDirSettings']['WORD_VARIABLES_FILE'])
+    WORD_TEMPLATE_FILE = os.path.join(
+        ACTIVE_DIR, settings['activeDirSettings']['WORD_TEMPLATE_FILE'])
+    WORD_VARIABLES_FILE = os.path.join(
+        ACTIVE_DIR, settings['activeDirSettings']['WORD_VARIABLES_FILE'])
     attachmentBool = True
 else:
     attachmentBool = False
 
 if settings['activeDirSettings']['WORD_TEMPLATE_FILE'] != None:
     df = pandas.read_excel(WORD_VARIABLES_FILE, index_col=0,
-                            keep_default_na=False)
+                           keep_default_na=False)
 
 
 def readXmlTemplate():
@@ -51,7 +56,7 @@ def createfiles():
     dictdata = df.to_dict()
     documents = {}
     template = readXmlTemplate()
-    outputdirname = datetime.datetime.now().strftime('%m-%d-%Y %H %M %S')
+    outputdirname = datetime.datetime.now().strftime('%d-%m-%Y %H %M %S')
     os.mkdir(os.path.join(ACTIVE_DIR, outputdirname))
     for country, dict_country in dictdata.items():
         documents[country] = template
@@ -59,7 +64,7 @@ def createfiles():
             documents[country] = documents[country].replace(
                 f'<!-- {key_translation}-->', str(value_translation))
         if attachmentBool:
-            base64pdf = wordTemplateToBase64(country,outputdirname)
+            base64pdf = wordTemplateToBase64(country, outputdirname)
             documents[country] = documents[country].replace(
                 '<!-- ATTACHMENT-->', base64pdf)
         filename = f'{country} - {XML_TEMPLATE_FILE_NAME}'
@@ -67,7 +72,7 @@ def createfiles():
             f.write(documents[country])
 
 
-def wordTemplateToBase64(localization,outputdirname):
+def wordTemplateToBase64(localization, outputdirname):
     with tempfile.TemporaryDirectory(prefix="peppolgenerator-") as tmpdir:
         dictdata = df[[localization]].to_dict()
         with zipfile.ZipFile(WORD_TEMPLATE_FILE, 'r') as zip_ref:
@@ -87,7 +92,8 @@ def wordTemplateToBase64(localization,outputdirname):
                   os.path.join(tmpdir, 'zipped.docx'))
         docx2pdf.convert(os.path.join(tmpdir, 'zipped.docx'),
                          os.path.join(tmpdir, 'base64encoded.pdf'))
-        shutil.copyfile(os.path.join(tmpdir, 'base64encoded.pdf'),os.path.join(ACTIVE_DIR, outputdirname, f'{localization}.pdf'))
+        shutil.copyfile(os.path.join(tmpdir, 'base64encoded.pdf'), os.path.join(
+            ACTIVE_DIR, outputdirname, f'{localization}.pdf'))
         with open(os.path.join(tmpdir, 'base64encoded.pdf'), "rb") as pdf_file:
             encoded_string = base64.b64encode(pdf_file.read())
         pure_string = encoded_string.decode('utf-8')
